66accdccb5d620fd8ea523587f3d8ce943e4decc
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2022-05-28 17:52:41 +0200

    Initial commit

---
+++ FSCareer/Start/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FSCareer.Start
+{
+	public class ViewModel
+	{
+		public ICommand Hyperlink { get; } = new Commands.CmdHyperlink();
+	}
+}
